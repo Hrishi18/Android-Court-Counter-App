2411b5d6a1b56fe4c15abb1dbc90ecbadb09abfc
Hrishikesh Gawas <hrishikesh.gawas99@gmail.com>
2020-06-23 18:41:54 +0530

    first commit

---
+++ app/src/main/java/com/example/android/courtcounter/FAMILYMEMBERS.java
@@ -0,0 +1,13 @@
+package com.example.android.courtcounter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FAMILYMEMBERS extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_familymembers);
+    }
+}
